f37eb0766fe27f7cf4a6e87abfd532ba95a9344f
tai <tai@localhost>
2012-02-04 22:00:44 +0900

    commit theme options framwork.

--- functions.php
+++ functions.php
@@ -308,3 +308,23 @@ add_filter( 'attachment_link', 'toolbox_enhanced_image_navigation' );
 /**
  * This theme was built with PHP, Semantic HTML, CSS, love, and a Toolbox.
  */
+
+if ( !function_exists( 'optionsframework_init' ) ) {
+
+	/*-----------------------------------------------------------------------------------*/
+	/* Options Framework Theme
+	/*-----------------------------------------------------------------------------------*/
+
+	/* Set the file path based on whether the Options Framework Theme is a parent theme or child theme */
+
+	if ( STYLESHEETPATH == TEMPLATEPATH ) {
+		define('OPTIONS_FRAMEWORK_URL', TEMPLATEPATH . '/admin/');
+		define('OPTIONS_FRAMEWORK_DIRECTORY', get_bloginfo('template_directory') . '/admin/');
+	} else {
+		define('OPTIONS_FRAMEWORK_URL', STYLESHEETPATH . '/admin/');
+		define('OPTIONS_FRAMEWORK_DIRECTORY', get_bloginfo('stylesheet_directory') . '/admin/');
+	}
+
+	require_once (OPTIONS_FRAMEWORK_URL . 'options-framework.php');
+
+}
